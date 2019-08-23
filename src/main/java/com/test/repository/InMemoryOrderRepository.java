2e3d51eef1bcee7c5443a0c99e1f831d113a78fb
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-08-23 07:32:00 +0100

    Improved tread safety.

--- src/main/java/com/test/repository/InMemoryOrderRepository.java
+++ src/main/java/com/test/repository/InMemoryOrderRepository.java
@@ -4,14 +4,14 @@ import com.test.model.Order;
 import com.test.repository.exception.OrderAlreadyStoredException;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryOrderRepository implements OrderRepository {
     private static final String EXCEPTION_MESSAGE = "Order with the given id already stored";
 
-    private final Map<String, Order> orders = new HashMap<>();
+    private final Map<String, Order> orders = new ConcurrentHashMap<>();
 
     // An alternative could be to overwrite the existing order
     @Override

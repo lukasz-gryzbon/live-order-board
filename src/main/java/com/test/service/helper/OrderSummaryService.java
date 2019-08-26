1b1278e3590cdc478e9de8aa1d7e2d1ee546c08c
Lukasz Gryzbon <lukasz.gryzbon@news.co.uk>
2019-08-26 15:43:39 +0100

    Refactor the logic to a more functional approach.

--- src/main/java/com/test/service/helper/OrderSummaryService.java
+++ src/main/java/com/test/service/helper/OrderSummaryService.java
@@ -1,53 +1,44 @@
 package com.test.service.helper;
 
-import static com.test.model.OrderType.BUY;
 import static com.test.model.OrderType.SELL;
+import static java.util.Collections.emptyList;
 import static java.util.Comparator.comparingInt;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 import com.test.model.Order;
 import com.test.model.OrderSummary;
 import com.test.model.OrderType;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class OrderSummaryService {
 
     private static final String MISSING_ARGUMENT_ORDERS_EXCEPTION_MESSAGE = "Missing argument: orders";
+    private static final Comparator<OrderSummary> byPriceASC = comparingInt(OrderSummary::getPrice);
 
     // Alternatively, this method could return a list, but then the frontend would need to separate the SELL & BUY data
-    public Map<OrderType, List<OrderSummary>> summarise(final List<Order> orders) {
-        validate(orders);
-        final Map<OrderType, List<Order>> ordersByType = groupByOrderType(orders);
-        return generateOrderSummary(ordersByType);
-    }
+    public Map<OrderType, List<OrderSummary>> summarise(final List<Order> allOrders) {
+        validate(allOrders);
+        final Map<OrderType, List<Order>> ordersByOrderType = allOrders.stream().collect(groupingBy(Order::getOrderType));
 
-    private Map<OrderType, List<Order>> groupByOrderType(final List<Order> orders) {
-        final Map<OrderType, List<Order>> ordersByType = orders.stream().collect(groupingBy(Order::getOrderType));
-        ordersByType.putIfAbsent(BUY, new ArrayList<>());
-        ordersByType.putIfAbsent(SELL, new ArrayList<>());
-        return ordersByType;
-    }
-
-    private Map<OrderType, List<OrderSummary>> generateOrderSummary(final Map<OrderType, List<Order>> ordersByType) {
-        final Map<OrderType, List<OrderSummary>> orderSummary = new HashMap<>();
-        orderSummary.put(BUY, generateOrderSummaries(ordersByType.get(BUY), BUY, comparingInt(OrderSummary::getPrice).reversed()));
-        orderSummary.put(SELL, generateOrderSummaries(ordersByType.get(SELL), SELL, comparingInt(OrderSummary::getPrice)));
-        return orderSummary;
-    }
-
-    private List<OrderSummary> generateOrderSummaries(final List<Order> orders, final OrderType orderType, final Comparator<OrderSummary> orderSummaryComparator) {
-        final Map<Integer, List<Order>> ordersByPrice = orders.stream().collect(groupingBy(Order::getPrice));
-
-        return ordersByPrice.keySet().stream()
-                .map(price -> new OrderSummary(ordersByPrice.get(price).stream().mapToDouble(Order::getQuantity).sum(), price, orderType))
-                .sorted(orderSummaryComparator)
-                .collect(toList());
+        return Stream.of(OrderType.values()).collect(
+                toMap(
+                        identity(), orderType ->
+                                ordersByOrderType.getOrDefault(orderType, emptyList()).stream().collect(
+                                        collectingAndThen(
+                                                groupingBy(Order::getPrice),
+                                                ordersByPrice -> ordersByPrice.entrySet().stream().
+                                                        map(priceAndOrdersEntry -> new OrderSummary(priceAndOrdersEntry.getValue().stream().mapToDouble(Order::getQuantity).sum(), priceAndOrdersEntry.getKey(), orderType)).
+                                                        sorted(orderType == SELL ? byPriceASC : byPriceASC.reversed()).
+                                                        collect(toList())
+                                        ))));
     }
 
     private void validate(final List<Order> orders) {

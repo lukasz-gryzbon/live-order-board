5dc088dbfdd980375e99eaab7a4c599c485b6088
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-05-17 12:32:46 +0100

    refactor: OrderSummaryService improved

--- src/main/java/com/test/service/helper/OrderSummaryService.kt
+++ src/main/java/com/test/service/helper/OrderSummaryService.kt
@@ -3,34 +3,21 @@ package com.test.service.helper
 import com.test.model.Order
 import com.test.model.OrderSummary
 import com.test.model.OrderType
+import com.test.model.OrderType.SELL
 
 class OrderSummaryService {
     // Alternatively, this method could return a list, but then the frontend would need to separate the SELL & BUY data
-    fun summarise(allOrders: List<Order>?): Map<OrderType, List<OrderSummary>> {
-        validate(allOrders)
-        val ordersByOrderType = allOrders!!.groupBy { it.orderType }
+    fun summarise(allOrders: List<Order>): Map<OrderType, List<OrderSummary>> {
+        val ordersByOrderType = allOrders.groupBy { it.orderType }
 
         val associateBy = OrderType.values().associateWith { orderType ->
-            (ordersByOrderType[orderType] ?: emptyList()).groupBy { order: Order -> order.price }
-                .map { entry ->
-                    OrderSummary(
-                        entry.value.fold(0.0) { acc, order -> acc + order.quantity },
-                        entry.key,
-                        orderType
-                    )
-                }
-                .sortedBy { summary: OrderSummary -> if (summary.orderType == OrderType.SELL) summary.price else -1 * summary.price }
+            (ordersByOrderType[orderType] ?: emptyList())
+                .groupingBy { order -> order.price }
+                .fold(0.0) { totalQuantity, order -> totalQuantity + order.quantity }
+                .map { priceToQuantity -> OrderSummary(priceToQuantity.value, priceToQuantity.key, orderType) }
+                .sortedBy { summary -> if (summary.orderType == SELL) summary.price else -1 * summary.price }
         }
 
-
         return associateBy
-    }
-
-    private fun validate(orders: List<Order>?) {
-        requireNotNull(orders) { MISSING_ARGUMENT_ORDERS_EXCEPTION_MESSAGE }
-    }
-
-    companion object {
-        private const val MISSING_ARGUMENT_ORDERS_EXCEPTION_MESSAGE = "Missing argument: orders"
     }
 }
